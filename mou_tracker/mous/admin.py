c549364e06b1ca58c6c71d85926b8b721ced32b9
Balakrishnan <139837926+bala-06@users.noreply.github.com>
2025-01-27 20:13:20 +0530

    Delete mou_tracker directory

--- mou_tracker/mous/admin.py
+++
@@ -1,9 +0,0 @@
-# mous/admin.py
-from django.contrib import admin
-from .models import Department, Organization, Outcome, MOU, Activity
-
-admin.site.register(Department)
-admin.site.register(Organization)
-admin.site.register(Outcome)
-admin.site.register(MOU)
-admin.site.register(Activity)
--- mou_tracker/mous/urls.py
+++
@@ -1,9 +0,0 @@
-# mous/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.mou_list, name='mou_list'),
-    path('upload/', views.upload_mou, name='upload_mou'),
-    path('activity/<int:activity_id>/complete/', views.complete_outcome, name='complete_outcome'),
-]
